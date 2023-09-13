62e96631ce0d579d5246e206219ccb480469b616
Jeffrey Nelson <49223599+jeffnelson1@users.noreply.github.com>
2023-09-13 12:07:17 -0500

    Delete aws_backup directory

--- aws_backup/kms_keys.tf
+++
@@ -1,9 +0,0 @@
-resource "aws_kms_key" "backup_vault" {
-  description             = var.backup_vault_key_desc
-  deletion_window_in_days = 10
-}
-
-resource "aws_kms_alias" "backup_vault" {
-  name          = "alias/${var.backup_vault_key_name}"
-  target_key_id = aws_kms_key.backup_vault.key_id
-}
